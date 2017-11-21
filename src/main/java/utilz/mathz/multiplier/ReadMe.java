9e9c829481635e699906697c020ea8ccdc6ceb42
DraxDomax <tk1986fe@gmail.com>
2017-11-21 19:59:45 +0000

    Changed readme

--- src/main/java/utilz/mathz/multiplier/ReadMe.java
+++ src/main/java/utilz/mathz/multiplier/ReadMe.java
@@ -3,7 +3,7 @@ package utilz.mathz.multiplier;
 public class ReadMe {
 
 	public static void main(String[] args) {
-        System.out.println( "You have read me. " );
+        System.out.println( "You have read me. You have read me. You have read me. You have read me. You have read me. You have read me. You have read me. You have read me. " );
 	}
 
 }
